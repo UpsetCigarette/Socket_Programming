3887c1e587f20c6e0c4570847e28cd0e348bc1ae
Joseph Grecco II <josephgreccoii@gmail.com>
2020-11-12 19:18:12 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,17 @@
 """
 Server side.
 """
+import socket
+
+server_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Server runs off teh local host
+server_soc.bind((socket.gethostname(), 1234))
+
+#Server listens for clients
+server_soc.listen(5)
+while True:
+#Server accepts client connections
+	clientsocket, address = server_soc.accept()
+	print(f"Connection from {address} has been created!")
+	clientsocket.send(bytes("Hi There", "utf-8"))
+
