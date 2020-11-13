42cfdbd52fa0a8e229ad347bc12c6d429ec23e57
Joseph Grecco II <josephgreccoii@gmail.com>
2020-11-12 19:17:55 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,12 @@
 """
 Client Side
 """
+import socket
+
+client_soc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client_soc.connect((socket.gethostname(), 1234))
+
+#The client will receive mesages using this code
+msg = client_soc.recv(1024)
+print(msg.decode("utf-8"))
