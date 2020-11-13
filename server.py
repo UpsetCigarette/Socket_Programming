df21ff0884e1a11d45057ce012c039991f10497b
Joseph Grecco II <josephgreccoii@gmail.com>
2020-11-12 19:28:23 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,51 @@
 """
 Server side.
 """
-import socket
+import socket, sys, threading
+from threading import Thread
 
-server_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Server runs off teh local host
-server_soc.bind((socket.gethostname(), 1234))
+users = []
+class User(object):
+    pass
 
-#Server listens for clients
-server_soc.listen(5)
+#@ make the TCP socket and put it in the variable sock
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = int(sys.argv[1])
+#@ bind sock on the default ip address, with the port found in port
+sock.bind(("127.0.0.1", port))
+sock.listen(1)
+
+def handle_connection(conn,info):
+    user = User()
+    users.append(user)
+    user.info = info
+    user.conn = conn
+    #@ send a "What is your name?" prompt on the connection conn
+    user.conn.sendall(b"What is your name?")
+    name = conn.recv(1024)
+    user.name = name
+    msgToSend = "Welcome " + str(user.name) + "!"
+    #@ send a welcome message along conn that includes the name that was received
+    user.conn.sendall(msgToSend.encode())
+    try:
+        while True:
+            #@ receive some data from conn
+            data = user.conn.recv(1024)
+            for other in users:
+
+                if other == user:
+                    continue
+                try:
+                    other.conn.sendall(name+b": "+data)
+                except:
+                    users.remove(other)
+    except:
+        #@ close conn
+        user.conn.close()
+        try:
+            del users[info]
+        except:
+            pass
+
 while True:
-#Server accepts client connections
-	clientsocket, address = server_soc.accept()
-	print(f"Connection from {address} has been created!")
-	clientsocket.send(bytes("Hi There", "utf-8"))
-
+    Thread(target=handle_connection,args=sock.accept()).start()
